ae38e3a01e31f735c0a1ead15c08040d656c4397
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-22 11:44:01 +0530

    ✨Add color helper for generating palettes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Palette from './components/Palette';
 import seedColors from './colors/seedColors';
+import { generatePalette } from './utils/colorHelpers';
 
 function App() {
+    console.log(generatePalette(seedColors[1]));
     return (
         <div className='App'>
             <Palette {...seedColors[1]} />
