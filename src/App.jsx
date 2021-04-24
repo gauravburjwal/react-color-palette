117bec380fdaa11fa3da8400386f109044078d6b
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-24 16:15:01 +0530

    🎇Add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import seedColors from './colors/seedColors';
 import { generatePalette } from './utils/colorHelpers';
 
@@ -5,9 +7,23 @@ import Palette from './components/Palette';
 
 function App() {
     return (
-        <div className='App'>
-            <Palette palette={generatePalette(seedColors[7])} />
-        </div>
+        <Router>
+            <Switch>
+                <Route
+                    exact
+                    path='/'
+                    render={() => <h1>Hello from Home Page</h1>}
+                />
+                <Route
+                    exact
+                    path='/palette/:id'
+                    render={() => <h1>Individual Palette</h1>}
+                />
+                <div>
+                    <Palette palette={generatePalette(seedColors[7])} />
+                </div>
+            </Switch>
+        </Router>
     );
 }
 
