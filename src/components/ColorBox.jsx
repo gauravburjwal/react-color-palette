bb64bb8ed78c5629961b3f3551fb89be4a52fbe7
Gaurav Burjwal <gauravburjwal@gmail.com>
2021-04-25 14:02:27 +0530

    🎇Adjust text color based on luminanse

--- src/components/ColorBox.jsx
+++ src/components/ColorBox.jsx
@@ -1,8 +1,11 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
+import chroma from 'chroma-js';
+
 import '../styles/ColorBox.css';
-import { Link } from 'react-router-dom';
 
 class ColorBox extends Component {
     constructor(props) {
@@ -25,6 +28,9 @@ class ColorBox extends Component {
         const { background, name, moreUrl, showLink } = this.props;
         const { isCopied } = this.state;
 
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() >= 0.7;
+
         return (
             <CopyToClipboard text={background} onCopy={this.copyChangeState}>
                 <div style={{ background }} className='ColorBox'>
@@ -34,20 +40,36 @@ class ColorBox extends Component {
                     />
                     <div className={`copy-msg ${isCopied && 'show'}`}>
                         <h1>copied!!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && 'dark-text'}>
+                            {background}
+                        </p>
                     </div>
                     <div className='copy-container'>
                         <div className='box-content'>
-                            <span>{name}</span>
+                            <span className={isDarkColor && 'light-text'}>
+                                {name}
+                            </span>
                         </div>
-                        <button className='copy-button'>Copy</button>
+                        <button
+                            className={`copy-button ${
+                                isLightColor && 'dark-text'
+                            }`}
+                        >
+                            Copy
+                        </button>
                     </div>
                     {showLink && (
                         <Link
                             to={moreUrl}
                             onClick={(evt) => evt.stopPropagation()}
                         >
-                            <span className='see-more'>MORE</span>
+                            <span
+                                className={`see-more ${
+                                    isLightColor && 'dark-text'
+                                }`}
+                            >
+                                MORE
+                            </span>
                         </Link>
                     )}
                 </div>
